02d1ffcc4d6d032339db41cee4bbfdce8e144a16
Sayani Koley <38989758+sayani2014@users.noreply.github.com>
2021-06-21 14:34:38 +0530

    Details added to understand the problem statement

--- InventoryManagement/src/InventoryController/InventoryMain.java
+++ InventoryManagement/src/InventoryController/InventoryMain.java
@@ -1,3 +1,15 @@
+/**
+ * User input of inventory name, price and weight.
+ * Add it in the json array.
+ * Edit the inventory to change weight and price.
+ * Delete the inventory.
+ * Print the json array in the console.
+ * Calculate the total weight, total price and total value of the inventory stored.
+ *
+ * @author: SAYANI KOLEY
+ * @since: 20.06.2021
+ */
+
 package InventoryController;
 
 import InventoryService.InventoryService;
